7ac72ca6ee25ffe330288d635123deff05bc0fed
saluvalasaichandu <saluvalasaichand@example.com>
2025-06-04 18:39:12 +0530

    terra

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,29 +1,34 @@
+# 1. Key Pair
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
   public_key = file("terra-key.pub")
 }
 
+# 2. Default VPC
 resource "aws_default_vpc" "default" {
-
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
+# 3. Get default subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
+}
+
+# 4. Security Group
 resource "aws_security_group" "allow_user_to_connect" {
-  name        = "allow TLS"
+  name        = "allow_tls"
   description = "Allow user to connect"
   vpc_id      = aws_default_vpc.default.id
+
   ingress {
     description = "port 22 allow"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = " allow all outgoing traffic "
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -43,21 +48,33 @@ resource "aws_security_group" "allow_user_to_connect" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    description = "allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "mysecurity"
   }
 }
 
+# 5. EC2 Instance
 resource "aws_instance" "testinstance" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  key_name        = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.allow_user_to_connect.name]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.allow_user_to_connect.id]
+
   tags = {
     Name = "Automate"
   }
+
   root_block_device {
-    volume_size = 30 
+    volume_size = 30
     volume_type = "gp3"
   }
 }
