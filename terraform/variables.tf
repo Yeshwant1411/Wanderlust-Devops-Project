67c6a6012658791feb1828c6384effda1ef1fc37
saluvalasaichandu <saluvalasaichand@example.com>
2025-06-04 18:51:02 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-084568db4383264d4"
+  default     = "ami-04f167a56786e4b09"
 }
 
 variable "instance_type" {
